918ebbcc778d742df244716f495592790b06a2e0
preshpi <egwuenuprecious7@gamil.com>
2023-01-11 12:22:22 +0100

    fix: folder structure

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -7,6 +7,7 @@ const Layout = () => {
   return (
   <div>
     <Navbar/>
+    <Outlet/>
     <Footer/>
   </div>
   )
--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Error = () => {
-  return <div>Error</div>;
+  return (
+  <div>
+            <h1 className='text-3xl font-bold'>PAGE NOT FOUND <code className='text-3xl font-bold'>404</code></h1>
+  </div>
+  )
 };
 
 export default Error;
